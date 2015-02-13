770cfd13c9706cf0f94bd71d77115a9eeb75db48
Beattle <ea29db6e18>
2015-02-13 18:20:49 +0300

    fresh_commit

---
+++ catalog/controller/product/bestseller.php
@@ -0,0 +1,175 @@
+<?php
+class ControllerProductBestseller extends Controller {
+	public function index() {
+		$this->language->load('product/bestseller');
+		
+      	$this->data['heading_title'] = $this->language->get('heading_title');
+
+		$this->data['button_cart'] = $this->language->get('button_cart');
+
+		$this->load->model('catalog/product');
+
+		$this->load->model('tool/image');
+
+		if (isset($this->request->get['page'])) {
+			$page = $this->request->get['page'];
+		} else {
+			$page = 1;
+		}
+
+		$limit = $this->config->get('config_catalog_limit');
+        //$limit = $this->config->get('module_limit');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->data['breadcrumbs'] = array();
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('text_home'),
+			'href'      => $this->url->link('common/home'),
+      		'separator' => false
+   		);
+
+		$url = '';
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+   		$this->data['breadcrumbs'][] = array(
+       		'text'      => $this->language->get('heading_title'),
+			'href'      => $this->url->link('product/bestseller', $url),
+      		'separator' => $this->language->get('text_separator')
+   		);
+
+		$this->data['products'] = array();
+
+		$data = array(
+			'sort'  => 'p.date_added',
+			'order' => 'DESC',
+			'start' => ($page - 1) * $limit,
+			'limit' => $limit
+		);
+
+
+
+        $results = $this->model_catalog_product->getBestSellerProducts(1000);
+        $product_total = count($results);
+
+		foreach ($results as $result) {
+			if ($result['image']) {
+				$image = $this->model_tool_image->resize($result['image'], $this->config->get('config_image_product_width'), $this->config->get('config_image_product_height'));
+			} else {
+				$image = false;
+			}
+
+			if (($this->config->get('config_customer_price') && $this->customer->isLogged()) || !$this->config->get('config_customer_price')) {
+				$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')));
+			} else {
+				$price = false;
+			}
+
+			if ((float)$result['special']) {
+				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')));
+			} else {
+				$special = false;
+			}
+
+			if ($this->config->get('config_review_status')) {
+				$rating = (int)$result['rating'];
+			} else {
+				$rating = false;
+			}
+
+				$cut_descr_symbols = 120;
+				$descr_plaintext = strip_tags(html_entity_decode($result['short_description'], ENT_QUOTES, 'UTF-8'));
+				if( mb_strlen($descr_plaintext, 'UTF-8') > $cut_descr_symbols )
+				{
+					$descr_plaintext = mb_substr($descr_plaintext, 0, $cut_descr_symbols, 'UTF-8') . '&nbsp;&hellip;';
+				}
+
+                $cut_descr_symbols = 50;
+                $name_plaintext    = $result['name'];
+          		if( mb_strlen($name_plaintext, 'UTF-8') > $cut_descr_symbols )
+          		{
+          			$name_plaintext = mb_substr($name_plaintext, 0, $cut_descr_symbols, 'UTF-8') . '&nbsp;&hellip;';
+          		}
+
+    			$sales_count = $result['sales_count'];
+    			$status_hit = $result['status_hit'];
+	    		$status_new = $result['status_new'];
+
+                if ($sales_count >= (int)$this->config->get('config_hit_status_limit')) {
+                    $status_new = 0;
+                    $status_hit = 1;
+                }
+                if ($status_new) {
+                    $status_hit = 0;
+                }
+
+            $this->data['products'][] = array(
+				'product_id'  => $result['product_id'],
+				'thumb'   	  => $image,
+				'name'    	  => $name_plaintext,
+				'title'    	  => $result['name'],
+				'description' => $descr_plaintext,
+				'price'   	  => $price,
+				'special' 	  => $special,
+				'rating'      => $rating,
+    			'status_hit'  => $status_hit,
+    			'status_new'  => $status_new,
+				'reviews'     => sprintf($this->language->get('text_reviews'), (int)$result['reviews']),
+				'href'    	  => $this->url->link('product/product', 'product_id=' . $result['product_id']),
+			);
+		}
+
+    	$url = '';
+
+    	if (isset($this->request->get['sort'])) {
+    		$url .= '&sort=' . $this->request->get['sort'];
+    	}
+
+    	if (isset($this->request->get['order'])) {
+    		$url .= '&order=' . $this->request->get['order'];
+    	}
+
+    	if (isset($this->request->get['limit'])) {
+    		$url .= '&limit=' . $this->request->get['limit'];
+    	}
+
+    	$pagination = new Pagination();
+    	$pagination->total          = $product_total;
+    	$pagination->page           = $page;
+    	$pagination->limit          = $limit;
+    	$pagination->text           = $this->language->get('text_total_page');
+        $pagination->style_links    = '';
+        $pagination->style_results  = '';
+        $pagination->text_first     = $this->language->get('text_first');
+        $pagination->text_last      = $this->language->get('text_last');
+        $pagination->text_next      = $this->language->get('text_next');
+        $pagination->text_prev      = $this->language->get('text_prev');
+
+    	$pagination->url = $this->url->link('product/bestseller', $url . '&page={page}');
+
+    	$this->data['pagination'] = $pagination->render();
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/product/bestseller.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/product/bestseller.tpl';
+		} else {
+			$this->template = 'default/template/product/bestseller.tpl';
+		}
+
+      	$this->children = array(
+      		'common/column_left',
+      		'common/column_right',
+			'common/column_center',
+      		'common/content_top',
+      		'common/content_bottom',
+      		'common/footer',
+      		'common/header'
+      	);
+
+      	$this->response->setOutput($this->render());
+	}
+}
+?>
---
+++ catalog/language/russian/product/bestseller.php
@@ -0,0 +1,14 @@
+<?php
+// Heading 
+$_['heading_title']	    = 'Хиты продаж';
+
+// Text
+$_['text_reviews']	    = 'На основании %s отзывов.';
+
+$_['text_total_page']   = 'Всего страниц: <b>{pages}</b>';
+$_['text_first']        = '&#8592;';
+$_['text_last']         = '&#8594;';
+$_['text_next']         = 'Вперед';
+$_['text_prev']         = 'Назад';
+
+?>
